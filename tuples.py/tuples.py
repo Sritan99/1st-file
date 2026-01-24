65845b2bfd504068cbb68202097db646e2f80b2b
Sritan99 <sritan.garapati@gmail.com>
2026-01-24 17:59:16 +0000

    Add files via upload

---
+++ tuples.py/tuples.py
@@ -0,0 +1,45 @@
+address=('1','glyme close','oxfordshire','Abingdon','England')
+
+for x in address:
+    print(x, end=' ')
+    
+
+house_num,appart_name,county,town,country=address
+
+print()
+print("house_num" ,house_num)
+
+print("appart_name",appart_name)
+
+print("county",county)
+print("town",town)
+print("country",country)
+
+
+my_tuple=3,4.6,"dog"
+
+print(my_tuple)
+
+n_tuple=("mouse",[8,4,6],(1,2,3))
+
+print(n_tuple[0][4])
+
+print(n_tuple[1][0])
+
+print(n_tuple[2][2])
+
+my_tuple=('a','b','c','d','e','f','g','h','i','j','k')
+
+print(my_tuple[1:4])
+
+print(my_tuple[:-7])
+
+print(my_tuple[7:])
+
+print(my_tuple[:])
+
+my_tuple=(1,2,3,4,5,6,7)
+print(my_tuple[-1])
+
+
+ 
